e117584ef20a2c6590ab176ba3a1808de3b81476
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-01-26 00:30:16 -0500

    settings

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken import views
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
